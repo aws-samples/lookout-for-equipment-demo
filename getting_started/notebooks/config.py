cc161a42d7bf2d21a5af68f9ddb41fd9e62e973b
Michael Hoarau <michoara@amazon.fr>
2021-08-31 13:54:47 +0000

    Added a configuration file and refreshed the notebooks

---
+++ getting_started/notebooks/config.py
@@ -0,0 +1,15 @@
+# Update the name of the bucket you want to use
+# to store the intermediate results of this getting
+# started:
+
+BUCKET                   = '<<YOUR_BUCKET>>'
+
+# You can leave these other parameters to these
+# default values:
+
+PREFIX_TRAINING          = 'demo/training-data/'
+PREFIX_LABEL             = 'demo/label-data/'
+PREFIX_INFERENCE         = 'demo/inference-data'
+DATASET_NAME             = 'demonstration'
+MODEL_NAME               = f'{DATASET_NAME}-model'
+INFERENCE_SCHEDULER_NAME = f'{DATASET_NAME}-scheduler'
---
+++ getting_started/utils/aws_matplotlib_light.py
@@ -0,0 +1,30 @@
+lines.linewidth: 1.0               # line width in points
+
+text.color: 232F3E
+
+axes.facecolor:     FAFAFA   # axes background color
+axes.edgecolor:     CACFD0   # axes edge color
+axes.linewidth:     1.5     # edge line width
+axes.grid:          True   # display grid or not
+axes.titlesize:     16   # font size of the axes title
+axes.titleweight:   bold  # font weight of title
+axes.titlecolor:    FF9900    # color of the axes title
+axes.labelcolor:    0073BB
+axes.prop_cycle: cycler('color', ['232F3E', '527FFF', '0073BB', '4d27aa', 'b0084d', 'bf0816', 'd45b07', 'FF9900', 'e38b00', '067f68', '16bf9f'])
+
+xtick.color:         0073BB   # color of the ticks
+xtick.labelsize:     12  # font size of the tick labels
+ytick.color:         0073BB   # color of the ticks
+ytick.labelsize:     12  # font size of the tick labels
+
+grid.color:     232F3E  # grid color
+grid.alpha:     0.1     # transparency, between 0.0 and 1.0
+
+legend.framealpha:    0.5      # legend patch transparency
+legend.facecolor:     fee385  # inherit from axes.facecolor; or color spec
+legend.edgecolor:     e38b00      # background patch boundary color
+legend.fancybox:      True     # if True, use a rounded box for the
+
+figure.titlesize:   large     # size of the figure title (``Figure.suptitle()``)
+figure.titleweight: bold    # weight of the figure title
+figure.facecolor:   F2F3F3     # figure face color
